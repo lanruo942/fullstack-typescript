2640b0b38b232dd35626e6423c51a2b68db35ca8
Summer Lee <lee@summer.today>
2022-08-02 11:12:46 +0800

    complete part9/9.9

--- patientor-backend/build/index.js
+++ patientor-backend/build/index.js
@@ -6,10 +6,10 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
     res.send('pong');
 });
-const PORT = 3002;
+const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- patientor-backend/index.ts
+++ patientor-backend/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+
 const app = express();
+
+const allowedOrigins = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+	origin: allowedOrigins,
+};
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
 app.use(express.json());
 
-app.get('/ping', (_req, res) => {
-	res.send('pong');
+app.get('/api/ping', (_req, res) => {
+	return res.send('pong');
 });
 
-const PORT = 3002;
+const PORT = 3001;
 
 app.listen(PORT, () => {
 	console.log(`Server running on port ${PORT}`);
